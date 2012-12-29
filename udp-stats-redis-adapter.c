1bab49e2eed5b5037f1ca9e56032f89a1f0f7430
Sam Rijs <sam@dev.local>
2012-12-29 21:55:52 +0100

    cleanup #1

--- udp-stats-redis-adapter.c
+++ udp-stats-redis-adapter.c
@@ -32,7 +32,7 @@ static inline struct sockaddr_in sock_hint (long addr, short port) {
   return hints;
 }
 
-static inline int parse_msg_parts (struct msg_parts *parts, char *msg, int n) {
+static inline int parse_msg (struct msg_parts *parts, char *msg, int n) {
   unsigned short j[16] = {0};
   int i;
   if (js0n((void *)msg, n, j) == 0) {
@@ -52,14 +52,14 @@ static inline int parse_msg_parts (struct msg_parts *parts, char *msg, int n) {
 
 int main (void) {
 
+  // Ignore pipe signals.
   signal(SIGPIPE, SIG_IGN);
 
   // Initiate UDP server socket.
   int sock = socket(AF_INET, SOCK_DGRAM, 0);
   struct sockaddr_in server = sock_hint(INADDR_ANY, 6667);
   if (bind(sock, (struct sockaddr *)&server, sizeof(server)) == -1) {
-    printf("Error binding socket.\n");
-    return 1;
+    goto err_bind;
   }
 
   printf("Now listening on UDP port %i...\n", 6667);
@@ -67,8 +67,8 @@ int main (void) {
   // Initiate redis client socket.
   int red = -1;
 
-  // Declare msg buffer and parts.
-  char msg[1024];
+  // Declare msg, send buffer and parts.
+  char msg[1024], send[1024];
   struct msg_parts msg_parts;
 
   // Declare small helper values.
@@ -91,42 +91,40 @@ int main (void) {
       red = socket(AF_INET, SOCK_STREAM, 0);
       struct sockaddr_in client = sock_hint(inet_addr("1.0.0.127"), 6379);
       if (connect(red, (struct sockaddr *)&client, sizeof(client)) == -1) {
-        printf("Error connecting to redis. Skipping...\n");
-        continue;
+        goto err_redis_connection;
       }
     }
 
     // Check and parse JSON message into parts.
     msg_parts = (struct msg_parts){NULL, NULL, NULL, NULL, 0, 0, 0, 0};
-    if (parse_msg_parts(&msg_parts, msg, n) < 0) {
-      printf("Malformed message. Skipping...\n");
-      continue;
+    if (parse_msg(&msg_parts, msg, n) < 0) {
+      goto err_json;
     }
 
     // If bucket is given, insert value into bucket.
     if (msg_parts.bucket) {
+
       // If the received timestamp is wrong, and there
       // was an error calling gettimeofday(), we exit.
       // Otherwise we use the time returned by gettimeofday.
       if (msg_parts.timestamp == NULL) {
         if (t == -1) {
-          printf("Error retrieving timestamp. Skipping...\n");
-          continue;
+          goto err_timestamp;
         }
         msg_parts.timestamp = &msg[n + 1];
         msg_parts.timestamp_len = snprintf(msg_parts.timestamp, 1024 - n - 1, "%.f", (double)tv.tv_sec * 1000 + (double)tv.tv_usec);
       }
-      char send[1024];
+
+      // Build and send redis query.
       n = snprintf(send, 1024, "ZADD stats:%s %s %i\r\n%s:%s\r\n",
                                msg_parts.bucket, msg_parts.timestamp,
                                msg_parts.timestamp_len + 1 + msg_parts.value_len,
                                msg_parts.timestamp, msg_parts.value);
       n = write(red, send, n);
       if (n == -1) {
-        printf("Error writing to redis. Skipping...\n");
-        red = -1;
-        continue;
+        goto err_redis_write;
       }
+
       // If kind is given, change kind of bucket.
       if (msg_parts.kind) {
         n = snprintf(send, 1024, "SET stats:%s:kind %i\r\n%s\r\n",
@@ -134,15 +132,32 @@ int main (void) {
                                  msg_parts.kind_len, msg_parts.kind);
         n = write(red, send, n);
         if (n == -1) {
-          printf("Error writing to redis. Skipping...\n");
-          red = -1;
-          continue;
+          goto err_redis_write;
         }
+
       }
+
     }
 
-  }
+    continue;
 
-  return 0;
+    err_bind:             printf("Error binding socket.       Exiting...\n");
+                          return 1;
+
+    err_redis_connection: printf("Error connecting to redis.  Skipping...\n");
+                          red = -1;
+                          continue;
+
+    err_redis_write:      printf("Error writing to redis.     Skipping...\n");
+                          red = -1;
+                          continue;
+
+    err_timestamp:        printf("Error retrieving timestamp. Skipping...\n");
+                          continue;
+
+    err_json:             printf("Error parsing json.         Skipping...\n");
+                          continue;
+
+  }
 
 }
