b89ada958acfcfac5d3d2f2e5b4e4a88e68a17c0
Sam Rijs <sam@dev.local>
2012-12-29 02:11:22 +0100

    factor part functionality into subfunctions

--- udp-stats-redis-adapter.c
+++ udp-stats-redis-adapter.c
@@ -8,17 +8,49 @@
 #include "credis.h"
 #include "parson.h"
 
-int main (void) {
+struct msg_parts {
+  JSON_Value *_json;
+  const char *bucket;
+  const char *type;
+  double      timestamp;
+  double      value;
+};
 
-  // Setup sockfd to be a DGRAM socket and bind
-  // to any:6667.
-  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+static inline int init_udp_socket(long addr, short port) {
+  int fd = socket(AF_INET, SOCK_DGRAM, 0);
   struct sockaddr_in server;
   memset(&server, 0, sizeof(server));
   server.sin_family      = AF_INET;
-  server.sin_addr.s_addr = htonl(INADDR_ANY);
-  server.sin_port        = htons(6667);
-  if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) == -1) {
+  server.sin_addr.s_addr = htonl(addr);
+  server.sin_port        = htons(port);
+  if (bind(fd, (struct sockaddr *)&server, sizeof(server)) == 0) {
+    return fd;
+  }
+  return -1;
+}
+
+static inline int parse_msg_parts(struct msg_parts *parts, const char *msg) {
+  JSON_Value *json = json_parse_string(msg);
+  if (json_value_get_type(json) == JSONObject) {
+    JSON_Object *jobj = json_value_get_object(json);
+    parts->bucket     = json_object_get_string(jobj, "bucket");
+    parts->type       = json_object_get_string(jobj, "type");
+    parts->timestamp  = json_object_get_number(jobj, "timestamp");
+    parts->value      = json_object_get_number(jobj, "value");
+    parts->_json      = json;
+    return 0;
+  }
+  return -1;
+}
+
+static inline void free_msg_parts (struct msg_parts *parts) {
+  json_value_free(parts->_json);
+}
+
+int main (void) {
+
+  int sock = init_udp_socket(INADDR_ANY, 6667);
+  if (sock < 0) {
     printf("Error binding socket.\n");
     return 1;
   }
@@ -27,16 +59,9 @@ int main (void) {
   // Declate Redis connection.
   REDIS red;
 
-  // Declare JSON values.
-  JSON_Value  *json;
-  JSON_Object *jobj;
-
   // Declare msg buffer and parts.
   char msg[1024];
-  const char *msg_bucket;
-  const char *msg_type;
-  double      msg_timestamp;
-  double      msg_value;
+  struct msg_parts msg_parts;
 
   // Declare small helper values.
   int    n, m;
@@ -48,7 +73,7 @@ int main (void) {
     // Receive a message of length `n` < 1024 into buffer `msg`,
     // null-terminate the received message and determine current
     // timestamp.
-    n      = recvfrom(sockfd, msg, 1023, 0, NULL, NULL);
+    n      = recvfrom(sock, msg, 1023, 0, NULL, NULL);
     msg[n] = '\0';
     tm     = time(NULL);
 
@@ -68,36 +93,28 @@ int main (void) {
       continue;
     }
 
-    // Parse the message and check for a root object.
-    json = json_parse_string(msg);
-    if (json_value_get_type(json) != JSONObject) {
+    // Check and parse JSON message into parts.
+    if (parse_msg_parts(&msg_parts, msg) < 0) {
       printf("Malformed message. Skipping...\n");
-      json_value_free(json);
+      free_msg_parts(&msg_parts);
       continue;
     }
 
-    // Get root object and retrieve values from it.
-    jobj = json_value_get_object(json);
-    msg_bucket    = json_object_get_string(jobj, "bucket");
-    msg_type      = json_object_get_string(jobj, "type");
-    msg_timestamp = json_object_get_number(jobj, "timestamp");
-    msg_value     = json_object_get_number(jobj, "value");
-
     // If bucket is given, insert value into bucket.
-    if (msg_bucket) {
-      msg_timestamp = msg_timestamp < 1 ? (double)tm : msg_timestamp;
-      n = snprintf(&msg[0],   512, "stats:%s", msg_bucket);
-      m = snprintf(&msg[512], 512, "%.0f:%f",  msg_timestamp, msg_value);
-      credis_zadd(red, &msg[0], msg_timestamp, &msg[512]);
+    if (msg_parts.bucket) {
+      msg_parts.timestamp = msg_parts.timestamp < 1 ? (double)tm : msg_parts.timestamp;
+      n = snprintf(&msg[0],   512, "stats:%s", msg_parts.bucket);
+      m = snprintf(&msg[512], 512, "%.0f:%f",  msg_parts.timestamp, msg_parts.value);
+      credis_zadd(red, &msg[0], msg_parts.timestamp, &msg[512]);
     }
 
     // If type is given, change type of bucket.
-    if (msg_bucket && msg_type) {
-      n = snprintf(msg, 1024, "stats:%s:type", msg_bucket);
-      credis_set(red, msg, msg_type);
+    if (msg_parts.bucket && msg_parts.type) {
+      n = snprintf(msg, 1024, "stats:%s:type", msg_parts.bucket);
+      credis_set(red, msg, msg_parts.type);
     }
 
-    json_value_free(json);
+    free_msg_parts(&msg_parts);
 
   }
 
